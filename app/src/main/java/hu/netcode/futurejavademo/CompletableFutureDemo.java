664e3be4c612c45ad3b7568336ba24d85a8f33b8
mobal <info@netcode.hu>
2021-07-24 13:58:58 +0200

    Refactor

--- app/src/main/java/hu/netcode/futurejavademo/CompletableFutureDemo.java
+++ app/src/main/java/hu/netcode/futurejavademo/CompletableFutureDemo.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import hu.netcode.futurejavademo.completablefuture.DuckDuckGo;
 import okhttp3.OkHttpClient;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.commons.lang3.time.StopWatch;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,8 +12,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.Duration;
-import java.time.Instant;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
@@ -23,27 +23,39 @@ public class CompletableFutureDemo {
     private static final Logger LOGGER = LogManager.getLogger(CompletableFutureDemo.class);
 
     public static void main(String[] args) {
-        final var start = Instant.now();
         final var objectMapper = new ObjectMapper().findAndRegisterModules();
         final var okHttpClient = new OkHttpClient();
+        final var stopWatch = new StopWatch();
         final var duck = new DuckDuckGo(objectMapper, okHttpClient);
+        stopWatch.start();
         try {
-            final List<CompletableFuture<String>> futureList = Files.readAllLines(
-                    Path.of(Objects.requireNonNull(CompletableFutureDemo.class.getClassLoader()
-                            .getResource("car_brands.txt")).toURI()))
-                    .stream()
-                    .map(duck::searchAsync)
-                    .collect(Collectors.toList());
+            final List<CompletableFuture<String>> futureList = CompletableFuture.supplyAsync(() -> {
+                try {
+                    return Files.readAllLines(
+                            Path.of(Objects.requireNonNull(CompletableFutureDemo.class.getClassLoader()
+                                    .getResource("car_brands.txt")).toURI()))
+                            .parallelStream()
+                            .map(duck::searchAsync)
+                            .collect(Collectors.toList());
+                } catch (IOException | URISyntaxException ex) {
+                    LOGGER.error(ExceptionUtils.getMessage(ex));
+                    return Collections.<CompletableFuture<String>>emptyList();
+                }
+            }).join();
             CompletableFuture.allOf(futureList.toArray(CompletableFuture[]::new))
-                    .thenApply(v -> futureList.stream().map(CompletableFuture::join).collect(Collectors.toList()))
+                    .thenApply(v -> futureList.parallelStream()
+                            .map(CompletableFuture::join)
+                            .collect(Collectors.toList())
+                    )
                     .join();
             futureList.forEach(f -> f.whenComplete((t, ex) -> {
                 if (ex != null) {
                     LOGGER.error(ExceptionUtils.getMessage(ex));
                 }
             }));
-            LOGGER.info("{} ms", Duration.between(start, Instant.now()).toMillisPart());
-        } catch (CompletionException | IOException | URISyntaxException ex) {
+            stopWatch.stop();
+            LOGGER.info("{} ms", stopWatch.getTime());
+        } catch (CompletionException ex) {
             LOGGER.error(ExceptionUtils.getMessage(ex));
             System.exit(-1);
         }
